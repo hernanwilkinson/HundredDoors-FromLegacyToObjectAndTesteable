83e1c6d470cc05afbb71296c2160debb49cff85b
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-07 18:26:20 -0300

    Cambio tipo de PrintStream a PrintWriter

--- src/HundredDoors.java
+++ src/HundredDoors.java
@@ -1,12 +1,15 @@
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class HundredDoors {
     // Ejemplo tomado de rosettacode: https://rosettacode.org/wiki/100_doors#With_an_array_of_boolean
     public static void main(String[] args) {
-        hundredDoors(System.out);
+        final PrintWriter out = new PrintWriter(System.out);
+        hundredDoors(out);
+        out.flush();
     }
 
-    private static void hundredDoors(PrintStream out) {
+    private static void hundredDoors(PrintWriter out) {
         boolean[] doors = new boolean[101];
 
         for (int i = 1; i < doors.length; i++) {
