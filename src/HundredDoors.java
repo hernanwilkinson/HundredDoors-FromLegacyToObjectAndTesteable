85d3df496a00e1b9c961c80e53388937bd8d869f
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-07 18:24:52 -0300

    Introduce Parameter out para System.out - Rompo acoplamiento

--- src/HundredDoors.java
+++ src/HundredDoors.java
@@ -1,10 +1,12 @@
+import java.io.PrintStream;
+
 public class HundredDoors {
     // Ejemplo tomado de rosettacode: https://rosettacode.org/wiki/100_doors#With_an_array_of_boolean
     public static void main(String[] args) {
-        hundredDoors();
+        hundredDoors(System.out);
     }
 
-    private static void hundredDoors() {
+    private static void hundredDoors(PrintStream out) {
         boolean[] doors = new boolean[101];
 
         for (int i = 1; i < doors.length; i++) {
@@ -15,7 +17,7 @@ public class HundredDoors {
 
         for (int i = 1; i < doors.length; i++) {
             if (doors[i]) {
-                System.out.printf("Door %d is open.%n", i);
+                out.printf("Door %d is open.%n", i);
             }
         }
     }
