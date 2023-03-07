65694947cc79f5385cc0e1b6e94c58d020ebf7bc
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-07 18:37:23 -0300

    Remove extra {}

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -6,15 +6,14 @@ class DoorsFlipper {
     }
 
     public boolean[] flipAll() {
-        for (int step = 1; step < doors.length; step++) {
+        for (int step = 1; step < doors.length; step++)
             flipEvery(step);
-        }
+
         return doors;
     }
 
     private void flipEvery(int step) {
-        for (int doorPosition = step; doorPosition < doors.length; doorPosition += step) {
+        for (int doorPosition = step; doorPosition < doors.length; doorPosition += step)
             doors[doorPosition] = !doors[doorPosition];
-        }
     }
 }
