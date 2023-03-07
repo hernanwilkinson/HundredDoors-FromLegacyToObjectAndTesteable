487c44c810aebb00e355cc9bbd89fb7453eb5a86
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-07 18:33:49 -0300

    Introduce Field doors

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -1,12 +1,13 @@
 class DoorsFlipper {
     private int numberOfDoors;
+    private boolean[] doors;
 
     public DoorsFlipper(int numberOfDoors) {
         this.numberOfDoors = numberOfDoors;
     }
 
     public boolean[] invoke() {
-        boolean[] doors = new boolean[numberOfDoors + 1];
+        doors = new boolean[numberOfDoors + 1];
 
         for (int i = 1; i < doors.length; i++) {
             for (int j = i; j < doors.length; j += i) {
